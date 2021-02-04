2bc90c3a4cb07576a6e9b74cee5c78c6f6d23e5e
blackhat hacker <71082274+samay826@users.noreply.github.com>
2021-02-04 15:00:46 +0800

    Add files via upload

---
+++ CEHcertificate.py
@@ -0,0 +1,70 @@
+# usr/bin/python
+# script by samay
+# author :- venom , carbon
+# CEH AND RED HAT , OSCP , OSEP :- samay has done CEH and venom bsdk red hat and carbon OSCP 
+'''
+bsdk vilen please yeh copy mat karna script
+NOOB HACKER LAVDE TU BHI
+'''
+import os
+print(".........WELCOME........")
+samay = input("DO YOU WANT TO MAKE CEH CERTIFICATE (YES/NO)")
+samay = str(samay)
+print('''  ██████  ▄▄▄       ███▄ ▄███▓ ▄▄▄     ▓██   ██▓
+▒██    ▒ ▒████▄    ▓██▒▀█▀ ██▒▒████▄    ▒██  ██▒
+░ ▓██▄   ▒██  ▀█▄  ▓██    ▓██░▒██  ▀█▄   ▒██ ██░
+  ▒   ██▒░██▄▄▄▄██ ▒██    ▒██ ░██▄▄▄▄██  ░ ▐██▓░
+▒██████▒▒ ▓█   ▓██▒▒██▒   ░██▒ ▓█   ▓██▒ ░ ██▒▓░
+▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░ ▒░   ░  ░ ▒▒   ▓▒█░  ██▒▒▒ 
+░ ░▒  ░ ░  ▒   ▒▒ ░░  ░      ░  ▒   ▒▒ ░▓██ ░▒░ 
+░  ░  ░    ░   ▒   ░      ░     ░   ▒   ▒ ▒ ░░  
+      ░        ░  ░       ░         ░  ░░ ░     
+                                        ░ ░     ''')
+print(".........WELCOME.........")
+vrushabh = "hello guys,\n keso ho sab"
+letter = ''' DEAR ,<|NAME|>
+YOU ARE ARRESTED UNDER 201 SECTION
+BSDK WAHI RUKH !!!!
+POLICE ARAHI HOGI 
+YOUR DATE :- <|DATE|>
+YOUR PHONE NUMBER :- <|NUMBER|>
+YOUR ADDRESS :- <|ADDRESS|>
+YOUR 2ND ADDRESS :- <|2NDADDRESS|>
+BSKWALO KO CERTIFICATE CHAIYE BHAGGO 
+BEHN KE LODE ..LUND LELE MERA '''
+vilen = input("ENTER YOUR NAME FOR CERTIFICATE\n")
+print(".......................")
+ishita = input("YOUR BIRTHDAY FOR CERTIFICATE\n")
+print(".......................")
+nikita = input("YOUR PHONE NUMBER FOR CERTIFICATE (WITHOUT +91)\n")
+print(".......................")
+neha = input("YOUR ADDRESS FOR CERTIFICATE\n")
+print(".......................")
+arpita = input("YOUR 2ND ADDRESS FOR BACKSIDE OF CERTIFICATE\n")
+print(".......................")
+print(".......................")
+print(''' __ __ _ ______ _____ __ _ ___ __
+/  |_ |_) |  | |_  | /  |_| | |_ 
+\__|__| \ | _|_|  _|_\__| | | |__ ''')
+print(".......................")
+print(".......................")
+vilen = str(vilen)
+nikita = str(nikita)
+neha = str(neha)
+arpita = str(arpita)
+letter = letter.replace("<|NAME|>",vilen)
+letter = letter.replace("<|DATE|>",ishita)
+letter = letter.replace("<|NUMBER|>",nikita)
+letter = letter.replace("<|ADDRESS|>",neha)
+letter = letter.replace("<|2NDADDRESS|>",arpita)
+print(letter)
+chutiya = ".......BAHGGO BHEN KE LODE......."
+print(chutiya)
+lavda = input("KYU BHAI SWADD AYA..? (YES/NO) :-")
+print(lavda)
+vrushabh = "vrushabh is good"
+print(len(vrushabh))
+vrushabh = vrushabh.replace("good","sexy")
+vrushabh = vrushabh.endswith("good")
+print(vrushabh)
+
